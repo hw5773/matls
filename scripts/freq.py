251e77c7aaa47d555de3f9a3ef7c3c40317ef8b6
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2017-10-06 10:26:12 +0900

    freq.py

--- scripts/freq.py
+++ scripts/freq.py
@@ -75,7 +75,7 @@ def write_freq(h):
 	of = open(ofname, "w")
 
 	for i in range(65536):
-		s = hex(i)[2:] + ", " + str(len(out[h][2][i])) + "\n"
+		s = hex(i)[4:] + ", " + str(len(out[h][4][i])) + "\n"
 		of.write(s)
 
 	of.close()

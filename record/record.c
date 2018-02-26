82a2b401b6a6372787cdcf929f5997a932569723
Hyunwoo Lee <hwlee2014@mmlab.snu.ac.kr>
2018-02-26 23:01:02 +0900

    modify record

---
+++ record/record.c
@@ -0,0 +1,16 @@
+/**
+ * @file record.c
+ * @author Hyunwoo Lee
+ * @date 26 Feb 2018
+ * @brief This file is to implement the functions to manipulate the
+ * modification record
+ */
+
+#include "record.h"
+
+int add_record(unsigned char *record, int rec_len, unsigned char *id, int id_len, unsigned char *mac_key, int mk_len, unsigned char *prev, int prev_len, unsigned char *next, int next_len)
+{
+  return SUCCESS;
+}
+
+int verify_record
--- record/record.h
+++ record/record.h
@@ -14,7 +14,11 @@
 #include <sys/time.h>
 #include <sys/socket.h>
 
-int A(int);
+struct record
+{
 
-int record_block();
+};
+
+int init_record();
+int add_record(unsigned char *record, int rec_len, unsigned char *id, int id_len, unsigned char *mac_key, int mk_len, unsigned char *prev, int prev_len, unsigned char *next, int next_len);
 int verify_block();
